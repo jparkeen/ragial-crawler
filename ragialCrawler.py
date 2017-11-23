94ec4c9060acce4802e55890fe1b76aa8102fea5
FelSiq <felipe.siqueira@usp.br>
2017-11-23 20:55:50 -0200

    Updating source code.

--- ragialCrawler.py
+++ ragialCrawler.py
@@ -1,53 +1,57 @@
 # -------------------- 1. IMPORT SECTION
-from urllib.request import Request, urlopen
-from termcolor import colored
-# from bs4 import BeautifulSoup
-import time
-import re
+from urllib.request import Request, urlopen # Necessary to communicate with Ragial
+from termcolor import colored # For good terminal print aesthetics
+from bs4 import BeautifulSoup # Because item page HTML is hell's scratch
+import time # Time, to put this daemon to sleep after a refresh
+import re # Regular expressions, to find interesting information
 # -------------------- END OF SECTION (1)
 
 # -------------------- 2. CONFIGURATION SECTION
 ragialServerLink = 'http://ragi.al/search/iRO-Odin/' 
 query = 'costume'
 myCustomHeader = {'User-Agent': 'Mozilla/5.0'}
-ragialRefreshTime = 600 # This time is in seconds
+ragialRefreshTime = 600 # This time should be in seconds
 # -------------------- END OF SECTION(2)
 
 # -------------------- 3. REGULAR EXPRESSIONS
 # Regex to search for the item links
-findItemURLRegex = re.compile(r'http://ragi\.al/item/iRO-Odin/([^"]+)')
-findItemTitleRegex = re.compile(r'<title>\s*([^<]+)\s*</title>')
+RegexfindItemURL = re.compile(r'http://ragi\.al/item/iRO-Odin/([^"]+)')
+# RegexfindItemTitle = re.compile(r'<title>\s*([^<]+)\s*</title>')
+RegexfindItemTitle = re.compile(r'<title>\s*Ragial\s*-\s*([^-]+)\s*-\s*iRO-Odin\s*</title>')
 # -------------------- END OF SECTION(3)
 
 # -------------------- 4. SOURCE SECTION
-# A greater loop here, to keep program running 'forever' 
-# Another Loop here, while it has a 'next' page to search up to
-rawPageSource = None
+# Outter loop here, to keep program running 'forever' daemon-like (1)
+# Inner Loop here, while it has a 'next' page to search up to (2)
+rawPageSource = str()
 try:
 	# Requesting Ragial Iro-Odin costume first page search
 	request = Request(ragialServerLink + query, headers = myCustomHeader)
-	rawPageSource = urlopen(request)
-except: 
-	print(colored('Fatal: could not get the source page from Ragial.', 'red'))
+	rawPageSource = str(urlopen(request).read())
+except BaseException as exc: 
+	print(colored('Fatal: could not get the source page from Ragial. (error: ' + repr(exc) + ')', 'red'))
 
 if rawPageSource:
-	# Using Beautiful soup for simplicity (not sure if it only make things slower)
-	# soupPageSource = BeautifulSoup(rawPageSource, 'html.parser').prettify()
-
 	# Find the item links, removing the identical ones
-	itemLinkID = list(set(findItemURLRegex.findall(str(rawPageSource.read()))))
+	itemLinkID = set(RegexfindItemURL.findall(rawPageSource))
 
 	# Get the best and average prices
-	for item in itemLinkID:
-		itemRawPageSource = None
-		try:
-			itemRequest = Request('http://ragi.al/item/iRO-Odin/' + item, headers = myCustomHeader)
-			itemRawPageSource = urlopen(itemRequest)
-			print(findItemTitleRegex.findall(itemRawPageSource.read()))
-		except:
-			print(colored('Error on getting', '\'http://ragi.al/item/iRO-Odin/' + item + '\'', 'data.', 'red'))
+	try:
+		for item in itemLinkID:
+			itemRawPageSource = str()
+			try:
+				itemRequest = Request('http://ragi.al/item/iRO-Odin/' + item, headers = myCustomHeader)
+				itemRawPageSource = str(urlopen(itemRequest).read())
+				print(RegexfindItemTitle.findall(itemRawPageSource))
+			except BaseException as exc:
+				print(colored('Error on getting', '\'http://ragi.al/item/iRO-Odin/' + item + '\' (error: ' + repr(exc) + ')', 'data.', 'red'))
+	except BaseException as exc:
+		print(colored('Error: ' + repr(exc), 'red'))
 
-	# Move to the next page, and repeat inner loop
-# Make a program 'sleep' for some minutes, to wait ragial update it's info
+	# Now sort the itens gathered by its commercial relevance and print all gathered data
+
+	# Move to the next page, and repeat inner loop (2)
+
+# Make the program 'sleep' for some minutes, to wait Ragial update it's info
 # time.sleep(ragialRefreshTime)
 # -------------------- END OF SECTION(4)
