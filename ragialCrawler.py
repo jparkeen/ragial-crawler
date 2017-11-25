2b0aaf578fafab827a0369e0fd46c356724a132f
FelSiq <felipe.siqueira@usp.br>
2017-11-25 01:17:00 -0200

    Added a Alpha Version of Source Code.

--- ragialCrawler.py
+++ ragialCrawler.py
@@ -1,3 +1,9 @@
+# -------------------- 0. INFORMATION
+"""
+
+"""
+# -------------------- END OF SECTION (0)
+
 # -------------------- 1. IMPORT SECTION
 from urllib.request import Request, urlopen # Necessary to communicate with Ragial
 from operator import itemgetter # To sort the information get
@@ -9,7 +15,7 @@ import re # Regular expressions, to find interesting information
 # -------------------- END OF SECTION (1)
 
 # -------------------- 2. CONFIGURATION SECTION
-ragialServerLink = 'http://ragi.al/search/iRO-Odin/' 
+ragialServerLink = 'http://ragi.al/search/iRO-Odin/'
 query = 'costume'
 myCustomHeader = {'User-Agent': 'Mozilla/5.0'}
 ragialRefreshTime = 600 # This time should be in seconds
@@ -34,9 +40,7 @@ class scriptInfoOrder(Enum):
 	PROPORTION = 0
 	ITEM_NAME = 1
 	MIN_CURRENT_PRICE = 2
-	MIN_SHORT = 3
 	AVG_SHORT = 4
-	MAX_SHORT = 5
 	
 # -------------------- END OF SECTION (3)
 
@@ -45,76 +49,114 @@ class scriptInfoOrder(Enum):
 RegexFindItemURL = re.compile(r'http://ragi\.al/item/iRO-Odin/([^"]+)')
 RegexFindItemTitle = re.compile(r'<title>\s*Ragial\s*-\s*([^-]+)\s*-\s*iRO-Odin\s*</title>')
 RegexFindItemPrice = re.compile(r'([0-9,]+)z')
+RegexFindNextPage = re.compile(r'<a href="' + ragialServerLink + query + '/' + r'\w">Next</a>')
 # -------------------- END OF SECTION (4)
 
 # -------------------- 5. SOURCE SECTION
+# 
+def _getItemName(itemRawPageSource):
+	regMatch = RegexFindItemTitle.search(itemRawPageSource)
+	return regMatch.group(1) if regMatch else 'Unknown Item Name'
+
+# 
+def _parseNewItem(itemTitle, itemRawPageSource):
+	# Seach for all relevant (zeny-based) information on the page HTML source code 
+	values = RegexFindItemPrice.findall(itemRawPageSource)
+	if values:
+		for i in range(len(values)):
+			values[i] = values[i].replace(',', '')
+		values = list(map(int, values))
+		
+		proportion = values[RagialValueOrder.MINIMAL_PRICE.value]/values[RagialValueOrder.AVG_SHORT_PERIOD.value] - 1
+		
+		return [proportion, itemTitle, values[RagialValueOrder.MINIMAL_PRICE.value], values[RagialValueOrder.AVG_SHORT_PERIOD.value]]
+	return [-1] * 4
+
+#
+def _mountQueryLink(pageIndex):
+	return ragialServerLink + query + '/' + str(pageIndex)
+
+# 
 def main():
 	# Outter loop to keep program running 'forever', daemon-like application (1)
 	while True:
-		# Inner Loop here, while it has a 'next' page to search up to (2)
-		rawPageSource = str()
-		try:
-			# Requesting Ragial Iro-Odin costume first page search
-			request = Request(ragialServerLink + query, 
-				headers = myCustomHeader)
-
-			rawPageSource = str(urlopen(request).read())
-
-		except BaseException as exc: 
-			print(colored('Fatal: could not get the source page from Ragial. (error: ' + repr(exc) + ')', 'red'))
-
+		# -------------------- 5.1 VARIABLE SETUP
+		pageIndex = 0
+		hasNextPage = True
 		# Start the information data structure. This is where all the information got goes before pandas data.frame
-		gatheredInfo = [[-1] * 6]
+		gatheredInfo = [[-1] * 4]
+		# -------------------- END OF SUBSECTION (5.1)
 
-		if rawPageSource:
-			# Find the item links, removing the identical ones
-			itemLinkID = set(RegexFindItemURL.findall(rawPageSource))
-
+		# Inner Loop here, while it has a 'next' page to search up to (2)
+		while hasNextPage:
+			rawPageSource = str()
+			pageIndex += 1
+			
+			# -------------------- 5.2 REQUEST RAGIAL HTML SOURCE
 			try:
-				for item in itemLinkID:
-					itemRawPageSource = str()
+				# Requesting Ragial Iro-Odin costume first page search
+				request = Request(_mountQueryLink(pageIndex), 
+					headers = myCustomHeader)
 
-					try:
-						itemRequest = Request('http://ragi.al/item/iRO-Odin/' + item, 
-							headers = myCustomHeader)
+				rawPageSource = str(urlopen(request).read())
 
-						# Get page HTML source code
-						itemRawPageSource = str(urlopen(itemRequest).read())
+			except BaseException as exc: 
+				print(colored('Fatal: could not get the source page from Ragial. (error: ' + repr(exc) + ')', 'red'))
+			# -------------------- END OF SUBSECTION (5.2)
 
-						# Search for the item name on the page HTML source code
-						itemTitle = RegexFindItemTitle.findall(itemRawPageSource)
-						# Seach for all relevant (zeny-based) information on the page HTML source code 
-						values = RegexFindItemPrice.findall(itemRawPageSource)
+			if rawPageSource:
+				# Find the item links, removing the identical ones
+				itemLinkID = set(RegexFindItemURL.findall(rawPageSource))
 
-						# Append a new information pack about a item
-						gatheredInfo.append([
-							values[scriptInfoOrder.PROPORTION.value],
-							values[scriptInfoOrder.ITEM_NAME.value],
-							values[scriptInfoOrder.MINIMAL_PRICE.value],
-							values[scriptInfoOrder.MIN_SHORT_PERIOD.value],
-							values[scriptInfoOrder.AVG_SHORT_PERIOD.value],
-							values[scriptInfoOrder.MAX_SHORT_PERIOD.value]
-							])
+				# -------------------- 5.3 GET ITEM ECONOMIC DATA
+				try:
+					for item in itemLinkID:
+						time.sleep(0.25)
+						itemRawPageSource = str()
 
-					except BaseException as exc:
-						print(colored('Error on getting', 
-							'\'http://ragi.al/item/iRO-Odin/' + item + '\' (error: ' + repr(exc) + ')', 
-							'data.', 'red'))
+						try:
+							itemRequest = Request('http://ragi.al/item/iRO-Odin/' + item, 
+								headers = myCustomHeader)
 
-			except BaseException as exc:
-				print(colored('Error: ' + repr(exc), 'red'))		
+							# Get page HTML source code
+							itemRawPageSource = str(urlopen(itemRequest).read())
 
-			# Move to the next page, and repeat inner loop (2)
+							# Search for the item name on the page HTML source code
+							itemTitle = _getItemName(itemRawPageSource)
 
+							# Append a new information pack about a item
+							gatheredInfo.append(_parseNewItem(itemTitle, itemRawPageSource))
+
+							print ('gatheredInfo:', gatheredInfo)
+
+						except BaseException as exc:
+							print(colored('Error on getting', 
+								'\'http://ragi.al/item/iRO-Odin/' + item + '\' (error: ' + repr(exc) + ')', 
+								'data.', 'red'))
+
+					# Delay to now overflow Ragial with requests
+					time.sleep(1.0)
+
+					# Move to the next page, and repeat inner loop (2)
+					hasNextPage = RegexFindNextPage.search(rawPageSource) 
+
+				except BaseException as exc:
+					print(colored('Error: ' + repr(exc), 'red'))
+					hasNextPage = False
+				# -------------------- END OF SUBSECTION (5.3)
+				
+		# -------------------- 5.4 PRINT ALL GATHERED INFORMATION
 		# Now sort the items gathered by its commercial relevance and print all gathered data
 		gatheredInfo.sort(key = itemgetter(scriptInfoOrder.PROPORTION.value), reverse = True)
 		dataFrame = pandas.DataFrame(gatheredInfo)
-		dataFrame.columns = ['P', 'Item Name', 'Best Price', 'Avg (7D)', 'Min (7D)', 'Max (7D)']
+		dataFrame.columns = ['P', 'Item Name', 'Best Price', 'Avg (7D)']
 		print(dataFrame)
+		# -------------------- END OF SUBSECTION (5.4)
 
 		# Make the program 'sleep' for some minutes, to wait Ragial update it's info
 		time.sleep(ragialRefreshTime)
 
+# SCRIPT START
 if __name__ == '__main__':
 	main()
 # -------------------- END OF SECTION (5)
